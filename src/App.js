65041ea5884c64ac69335da2968626c128761742
Your Name <you@example.com>
2020-07-01 10:48:11 -0400

    Commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Main from './components/Main/Main';
 import FormP from './components/SignUp/signup'
-import Landing from './components/Landing/Landing';
+import Speech from './components/Landing/Landing';
 import Contact from './components/Contact/Contact';
 import FormPage from './components/LogIn/login';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
@@ -14,7 +14,7 @@ function App() {
   return (
     <Router>
       <div>
-    <Route exact path="/" component ={Landing} />
+    <Route exact path="/" component ={Speech} />
     <Route exact path="/main" component ={Main} />
     <Route exact path="/contact" component ={Contact} />
     <Route exact path="/login" component ={FormPage} />
