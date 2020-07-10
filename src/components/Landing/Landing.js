d7aa2cd67682e7f7a36127bda8ac56bb641c5447
Your Name <you@example.com>
2020-07-10 00:26:45 -0400

    Commit

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -48,7 +48,7 @@ class Speech extends Component {
     } else {
       recognition.stop()
       recognition.onend = () => {
-        console.log("Stopped listening per click")
+        console.log("Stopped listening when click")
       }
     }
 
@@ -56,31 +56,21 @@ class Speech extends Component {
       console.log("Listening!")
     }
 
-    let finalTranscript = ''
     recognition.onresult = event => {
-      let interimTranscript = ''
+      var last = event.results.length - 1;
+      var command = event.results[last][0].transcript;
 
-      for (let i = event.resultIndex; i < event.results.length; i++) {
-        const transcript = event.results[i][0].transcript;
-        if (event.results[i].isFinal) finalTranscript += transcript + ' ';
-        else interimTranscript += transcript;
+      if (command.toLowerCase() === 'select English') {
+        return document.getElementById("mySelect").options.namedItem("English").text;
+        // document.getElementById("English").options[0] = true;
       }
-      document.getElementById('interim').innerHTML = interimTranscript
-      document.getElementById('final').innerHTML = finalTranscript
-
-      //-------------------------COMMANDS------------------------------------
-
-      const transcriptArr = finalTranscript.split(' ')
-      const stopCmd = transcriptArr.slice(-3, -1)
-      console.log('stopCmd', stopCmd)
-
-      if (stopCmd[0] === 'stop' && stopCmd[1] === 'listening') {
-        recognition.stop()
-        recognition.onend = () => {
-          console.log('Stopped listening per command')
-          const finalText = transcriptArr.slice(0, -3).join(' ')
-          document.getElementById('final').innerHTML = finalText
-        }
+      else if (command.toLowerCase() === 'Escolher Português') {
+        return document.getElementById("mySelect").options.namedItem("Português").text;
+        // document.getElementById("Português").selected = true;
+      }
+      else if (command.toLowerCase() === 'selecciona Spañol') {
+        return document.getElementById("mySelect").options.namedItem("Español").text;
+        // document.getElementById("Español").selected = true;
       }
     }
 
@@ -94,7 +84,7 @@ class Speech extends Component {
 
   render() {
     return (
-      <div  className={classes.All}>
+      <div className={classes.All}>
         <section className={classes.Showcase}>
           <div className={classes.VideoContainer}>
             <video src={Video} autoPlay muted loop></video>
@@ -105,26 +95,19 @@ class Speech extends Component {
               <img src={Logo} alt="logo" className={classes.LogoImg} onClick={this.toggleListen} />
               <h1>WikipediA</h1>
               <p>The Free Encyclopedia</p>
-              <Form.Control as="select" id='final'>
-                <option>English</option>
-                <option>Português</option>
-                <option>Español</option>
-                <option>Русский</option>
-                <option>日本語</option>
-                <option>Français</option>
-                <option>中文</option>
-                <option>Deutsch</option>
-                <option>Italiano</option>
-                <option>Polski</option>
+
+              <Form.Control as="select" id="mySelect" >
+                <option id="Português">Português</option>
+                <option id="English">English</option>
+                <option id="Español">Español</option>
+                <option id="Italiano">Italiano</option>
               </Form.Control>
+
             </div>
+            <div id='message'></div>
           </div>
         </section>
-        {/* <div>
-        <div id='interim' ></div>
-        <div id='final' ></div>
-      </div> */}
-
+        
         <section className={classes.Footer}><Footer />
         </section>
       </div>
